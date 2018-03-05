b486187d7a011db3f86914953670b64ea9703db1
namaoy2538 <namaoy999999@gmail.com>
2018-03-04 20:13:00 -0800

    myFirstJAVA

---
+++ src/myFirstJAVA/myFirstJAVA.java
@@ -0,0 +1,11 @@
+package myFirstJAVA;
+public class myFirstJAVA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Wordl, I'm SAM");
+        System.out.println("Information System");
+
+    }// main
+
+
+} // class
